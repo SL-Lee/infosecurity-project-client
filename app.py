7a303513840902090ca1f2be2f7eaf3d9faf53a5
SL-Lee <45960387+SL-Lee@users.noreply.github.com>
2020-05-28 13:08:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for, make_response, flash
-from flask_login import LoginManager, login_required, login_user, logout_user, current_user
+from flask import (
+    flash,
+    Flask,
+    make_response,
+    redirect,
+    render_template,
+    request,
+    url_for
+)
+from flask_login import (
+    current_user,
+    login_required,
+    login_user,
+    LoginManager,
+    logout_user
+)
 from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
 
 app.secret_key = os.urandom(16)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
 
 db = SQLAlchemy(app)
 
@@ -25,10 +39,22 @@ class User(db.Model):
     date_created = db.Column(db.DateTime, default=datetime.now)
 
 
-@app.route('/')
-def home():
-    return "Hello World"
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
 
-if __name__ == '__main__':
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
+
+
+if __name__ == "__main__":
     app.run()
