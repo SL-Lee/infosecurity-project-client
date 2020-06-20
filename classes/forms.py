194c709a6c9ac7288f500e11f9ba78719f5c2a01
Lim Wen Da <8577@hotmail.sg>
2020-06-20 17:02:07 +0800

    CRD of Users in admin view done

--- classes/forms.py
+++ classes/forms.py
@@ -21,3 +21,9 @@ class RegisterForm(Form):
     email = StringField("Email", [InputRequired(), Email("Please enter your email address"), Length(max=120)])
     username = StringField("Username", [InputRequired(), Length(min=4, max=25)])
     password = PasswordField("Password", [InputRequired(), Length(min=8, max=80)])
+
+
+class AdminCreateForm(Form):
+    email = StringField("Email", [InputRequired(), Email("Please enter your email address"), Length(max=120)])
+    username = StringField("Username", [InputRequired(), Length(min=4, max=25)])
+    password = PasswordField("Password", [InputRequired(), Length(min=8, max=80)])
