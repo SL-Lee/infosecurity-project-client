07ee55079cbbed2e895f0b583489851632e797c1
Lim Wen Da <8577@hotmail.sg>
2020-07-23 14:56:38 +0800

    add in more security hearders

--- app.py
+++ app.py
@@ -735,8 +735,10 @@ def search():
 
 @app.after_request
 def add_header(response):
+    response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
     response.headers["X-Content-Type-Options"] = "nosniff"
     response.headers["X-Frame-Options"] = "SAMEORIGIN"
+    response.headers['X-XSS-Protection'] = '1; mode=block'
     return response
 
 
